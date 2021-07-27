ab38383891b9b5aa2cbe54394e90157d2d03e2ff
Sahala <solomiia.sahala@sigmaukraine.com>
2021-07-27 10:28:09 +0300

    added contacts and favorite links in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,8 @@ const logoMobile = './images/logo-mobile.svg';
 const iconImportCSV = './images/importCSV.svg';
 
 const Header = () => {
+    const url = window.location.href.split('/');
+    let currentPath = url[url.length - 1];
     return (
         <header>
             <div className="header-container">
@@ -13,6 +15,12 @@ const Header = () => {
                         <source media="(min-width: 768px)" srcSet={logoDesktop} />
                         <img src={logoMobile} alt="logo" />
                     </picture>
+                </a>
+                <a href="/" className={`contacts ${currentPath === '' ? 'line' : ''}`}>
+                    Contacts
+                </a>
+                <a href="/favorites" className={`favorites ${currentPath === 'favorites' ? 'line' : ''}`}>
+                    Favorites
                 </a>
                 <a href="/importCSV" className="import-csv">
                     <img src={iconImportCSV} alt="icon import CSV" />
