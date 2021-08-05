0b599e144d71237a76563e92cdf42bb07638ef19
Sahala <solomiia.sahala@sigmaukraine.com>
2021-08-05 21:04:08 +0300

    added useEffect to Contact component

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
+
+import api from '../services/api';
+
+// type UserCardType = {
+//     nameSurname: string,
+//     mobile: number,
+//     email: string
+//     position?: string,
+//     job?: string,
+//     favorite: boolean,
+//     linkFacebook?: string,
+//     linkLinkedin?: string,
+//     linkTwitter?: string,
+// };
+
 const Contacts = () => {
+    const [users, setUsers] = useState<any[] | null>(null);
+    useEffect(() => {
+        api.getAllContacts().then((result) => setUsers(result))
+    }, [])
     return (
-        <div>
-            Contacts
-        </div>
+        <div className="contacts-container" >
+            <h3>Contacts({users?.length})</h3>
+        </div >
     )
 }
 
 export default Contacts;
-
