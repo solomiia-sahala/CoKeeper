cee372c86dabe570200ae9354606f86ffb67c644
Sahala <solomiia.sahala@sigmaukraine.com>
2021-08-10 22:37:00 +0300

    added edit page

---
+++ src/components/EditContact.tsx
@@ -0,0 +1,208 @@
+import { useState, useEffect, FormEvent, ChangeEvent } from 'react';
+import { useHistory, useParams } from 'react-router';
+import classNames from 'classnames';
+
+import api from '../services/api';
+
+import FavoriteIcon from './FavoriteIcon';
+import Button from './Button';
+
+const star = '/images/star.svg';
+
+const EditContact = () => {
+    const history = useHistory();
+    const { id } = useParams<{ id: string }>();
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<string>('');
+    const [userInfo, setUserInfo] = useState({
+        nameSurname: '',
+        mobile: undefined,
+        email: '',
+        avatar: '',
+        position: '',
+        jobTitle: '',
+        linkFacebook: '',
+        linkLinkedin: '',
+        linkTwitter: '',
+        favorite: false
+    });
+
+    useEffect(() => {
+        api.getContactById(id).then((res: any): void => {
+            setUserInfo(res);
+            setIsFavorite(res.favorite);
+            setSelectedImage(res.avatar)
+        });
+    }, [])
+
+    const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+    }
+
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files == null) {
+            throw new Error("Error finding e.target.files");
+        }
+        if (e.target.files[0]) {
+            setSelectedImage(URL.createObjectURL(e.target.files[0]))
+            setUserInfo({ ...userInfo, [e.target.name]: e.target.files[0] });
+        }
+
+    }
+
+    const onUpdateForm = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        api.updateContact(id, userInfo);
+        // history.push(`/aboutContact/${id}`);
+    }
+
+    const redirectToHome = (): void => {
+        history.push('/');
+    }
+
+    const changeFavoriteStatus = () => {
+        setIsFavorite(!isFavorite);
+        setUserInfo({ ...userInfo, "favorite": !userInfo.favorite });
+    }
+    const {
+        nameSurname,
+        mobile,
+        email,
+        avatar,
+        position,
+        jobTitle,
+        linkFacebook,
+        linkLinkedin,
+        linkTwitter,
+    } = userInfo;
+    return (
+        <div>
+            <div className="header-edit">
+                <h1>Edit contact</h1>
+            </div>
+            <form onSubmit={onUpdateForm}>
+                <div className="form">
+                    <div className="grid-container-general-info">
+                        <div className={classNames("img", { 'shadow': selectedImage })}>
+                            <label htmlFor="file-input">
+                                <img src={selectedImage} alt="no image" />
+                            </label>
+                            <input id="file-input" name="avatar" type="file" onChange={handleChangeImage} />
+                        </div>
+                        <div className="required-fields">
+                            <p>General info <img src={star} alt="required fields" className="star-icon" /></p>
+                            <input
+                                type="text"
+                                value={nameSurname}
+                                name="nameSurname"
+                                required
+                                placeholder="Name&Surname"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                            <br />
+                            <img src="/images/mobile-phone.svg" alt="icon" />
+                            <input
+                                type="number"
+                                value={mobile}
+                                name="mobile"
+                                required
+                                placeholder="Mobile phone"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                            <br />
+                            <img src="/images/email.svg" alt="icon" />
+                            <input
+                                type="email"
+                                value={email}
+                                name="email"
+                                required
+                                placeholder="Email"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                        </div>
+                        <FavoriteIcon favorite={isFavorite} callback={changeFavoriteStatus} />
+                    </div>
+                    <div className="grid-container-add-info">
+                        <div>
+                            <p>Social links</p>
+                            <img src="/images/facebook.svg" alt="icon" />
+                            <input
+                                type="text"
+                                value={linkFacebook}
+                                name="linkFacebook"
+                                placeholder="Link to Facebook"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                            <br />
+                            <img src="/images/twitter.svg" alt="icon" />
+                            <input
+                                type="text"
+                                value={linkTwitter}
+                                name="linkTwitter"
+                                placeholder="Link to Twitter"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                            <br />
+                            <img src="/images/linkedin.svg" alt="icon" />
+                            <input
+                                type="text"
+                                value={linkLinkedin}
+                                name="linkLinkedin"
+                                placeholder="Link to Linkedin"
+                                className="field"
+                                onChange={handleChangeInput} />
+                        </div>
+                        <div>
+                            <p>Job details</p>
+                            <img src="/images/location-company.svg" alt="icon" />
+                            <input
+                                type="text"
+                                name="jobTitle"
+                                value={jobTitle}
+                                placeholder="Job title"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                            <br />
+                            <img src="/images/work-case.svg" alt="icon" />
+                            <input
+                                type="text"
+                                value={position}
+                                name="position"
+                                placeholder="Position"
+                                className="field"
+                                onChange={handleChangeInput}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons">
+                    <div className="button-container">
+                        <Button
+                            type='button'
+                            value='Cancel'
+                            className='grey'
+                            src="./images/cancel.svg"
+                            callback={redirectToHome}
+                        />
+                    </div>
+                    <div className="button-container">
+                        <Button
+                            type='submit'
+                            value='Update'
+                            className='green'
+                            src="./images/ok.svg"
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default EditContact;
--- src/components/FavoriteIcon.tsx
+++ src/components/FavoriteIcon.tsx
@@ -2,17 +2,17 @@ import { useState } from 'react';
 
 import '../styles/FavoriteIcon.scss';
 
-const notFavoriteIcon = './images/notFavorite.svg';
-const isFavoriteIcon = './images/isFavorite.svg';
+const notFavoriteIcon = '/images/notFavorite.svg';
+const isFavoriteIcon = '/images/isFavorite.svg';
 
-const FavoriteIcon = ({ favorite }: { favorite: boolean }) => {
-    const [isFavorite, setIsFavorite] = useState(favorite);
+const FavoriteIcon = ({ favorite, callback }: { favorite: boolean, callback: Function }) => {
+    // const [isFavorite, setIsFavorite] = useState(favorite);
 
     return (
         <div className="favorite">
-            <img src={isFavorite ? isFavoriteIcon : notFavoriteIcon}
+            <img src={favorite ? isFavoriteIcon : notFavoriteIcon}
                 alt="favorite icon"
-                onClick={() => setIsFavorite(!isFavorite)}
+                onClick={() => callback()}
             />
         </div>
     )
